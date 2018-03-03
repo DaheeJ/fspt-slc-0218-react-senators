4d94fa0dea3db74c36acce32944668b87713ae95
Wes Cutting <wesleycutting@gmail.com>
2018-03-03 14:19:56 -0700

    Updating Senators Component

--- src/components/Senators.js
+++ src/components/Senators.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import senators from '../data/senators'
 
 export default class extends Component {
-  render () {
-    return (
+  renderSenators () {
+    return senators.map(senator =>
       <div>Test</div>
+    )
+  }
+  render () {
+    const senators = this.renderSenators();
+    return (
+      <div>
+        {senators}
+      </div>
     );
   }
 }
