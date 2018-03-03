c5f0b1bf8bcc81d130c7bb586870cf4ab2c6f2ec
Wes Cutting <wesleycutting@gmail.com>
2018-03-03 15:11:10 -0700

    Created Senator Component

--- src/components/Senators.js
+++ src/components/Senators.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import senators from '../data/senators'
+import Senator from './Senator'
 
 export default class extends Component {
   renderSenators () {
     return senators.map(senator =>
-      <div>Test</div>
+      <Senator senator={senator}/>
     )
   }
   render () {
-    const senators = this.renderSenators();
+    const senators = this.renderSenators()
+    
     return (
       <div>
         {senators}
